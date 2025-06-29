068ca7821add544929b94bac13400fed132aa5dd
araujobrunodev <ballogggamer@gmail.com>
2025-06-29 10:16:13 -0300

    Add welcome function

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+//This is the main file that run the whole software
+//This file will run the Answerhere's apresentation 
+
+/**
+ * @function welcome
+ * | Give you a welcome
+*/
+function welcome () {
+    console.log("\n========================")
+    console.log("Welcome to Answerhere")
+    console.log("========================\n")
+    console.log("This software is just to be a quiz.\n")
+}
+
+welcome()
