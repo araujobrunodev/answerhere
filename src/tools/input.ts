3473be3234e714ca1c8fa97b0f4463ed18bfdb01
araujobrunodev <ballogggamer@gmail.com>
2025-06-29 12:43:21 -0300

    Add input function

---
+++ src/tools/input.ts
@@ -0,0 +1,15 @@
+import {current_interface} from "./rl"
+
+/**
+ * @function input
+ * @param {string} msg
+ * | This will ask data                    
+ * | The parameter will receive a message and get your answer
+*/
+async function input (msg:string):Promise<string> {
+    return new Promise((resolve) => 
+        current_interface.question(msg, (answer) => resolve(answer))
+    )
+}
+
+export default input
