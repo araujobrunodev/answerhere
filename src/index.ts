3b88e126f89f15f148ae589d457e33ce62e7fea0
araujobrunodev <ballogggamer@gmail.com>
2025-06-29 14:10:10 -0300

    Add timer

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,19 @@
 //This file will run the Answerhere's apresentation 
 
 import routes from "./tools/routes"
+import timer from "./tools/timer"
 
 /**
  * @function welcome
  * | Give you a welcome
 */
-function welcome () {
+async function welcome () {
     console.log("\n========================")
     console.log("Welcome to Answerhere")
     console.log("========================\n")
     console.log("This software is just to be a quiz.\n")
 
+    await timer(4)
     routes("MAIN")
 }
 
