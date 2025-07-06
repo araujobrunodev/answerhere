f00e3696ef6ef5a6ea610970a1292696636d812a
araujobrunodev <ballogggamer@gmail.com>
2025-07-06 20:32:18 -0300

    Fix: fix repeated quiz in DB

--- src/storage/saveQuizzes.ts
+++ src/storage/saveQuizzes.ts
@@ -2,6 +2,7 @@ import { quizType } from "../structureType/questionsType"
 import * as fs from "fs"
 import timer from "../tools/timer"
 import { directory_path, quiz_list_path } from "./paths"
+import { quizList } from "./cache"
 
 if (!fs.existsSync(directory_path)) fs.mkdirSync(directory_path, {recursive: true})
 
@@ -21,7 +22,8 @@ async function saveQuizzes (quizzes: quizType[]) {
         else file_data_in_json = fs.readFileSync(quiz_list_path, {encoding: "utf-8"})
 
         let file_data = JSON.parse(file_data_in_json) as quizType[]
-        file_data = file_data.concat(quizzes)
+        const filter_repeated_quiz = new Set(file_data.map(quiz => quiz.ID))
+        file_data = file_data.concat(quizList.filter(quiz => !filter_repeated_quiz.has(quiz.ID)))
         
         const toJSON = JSON.stringify(file_data)
 
